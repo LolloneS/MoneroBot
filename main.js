a4b4fc16b465d7db2b2c81a57973a17b449c9ddc
Lorenzo Soligo <lorenzo.a7x@gmail.com>
2017-11-21 20:29:01 +0100

    changed APIs

--- main.js
+++ main.js
@@ -1,64 +1,85 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-const STRING_ERROR_KRAKEN = "Something's wrong with Kraken. Try again later";
-
+const https = require('https');
+const standardQuery = "https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=";
 const STRING_RESPONSE_START = "Welcome to the ultimate Monero Bot. For more information, contacts, etc: type /help :)";
-
+const STRING_RESPONSE_XMR = "<b>1 XMR = ";
 const STRING_RESPONSE_HELP =
          "Welcome to the ultimate Monero Bot.\n"
          + "Feel free to suggest me improvements and fixes @LolloneS. I'll try to get back to you as soon as I can.\n"
          + "<b>I know this isn't perfect</b>, I'm just an enthusiast trying to learn something :)\n"
          + "Wanna know how to use me? Just type /xmr :)";
-
-const STRING_RESPONSE_XMR = "<b>Value</b>: ";
-
-
-// Let's get kraken ready
-var KrakenClient = require('kraken-api');
-var kraken = new KrakenClient();
-
-// Telegram token you receive from @BotFather
-const token = 'YOUR_TOKEN';
-
-
-// Create a bot that uses 'polling' to fetch new updates
+const STRING_ERROR = "I'm sorry, there was a problem with the APIs. Please try again later. If the problem persists, let me [@LolloneS] know."
+const token = 'token';         
 const bot = new TelegramBot(token, {polling: true});
 
 
-// start
 bot.onText(/\/start/, (msg, match) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, STRING_RESPONSE_START, {parse_mode : "html"});
 });
 
-// help
+
 bot.onText(/\/help/, (msg, match) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, STRING_RESPONSE_HELP, {parse_mode : "html"});
 });
 
-bot.onText(/\/xmr/, (msg, match) => {
+
+bot.onText(/\/eur/, async (msg, match) => {
   const chatId = msg.chat.id;
-  krakenXMREURWrapped().then((a) => {
-    bot.sendMessage(chatId, STRING_RESPONSE_XMR + a + "€", {parse_mode : "html"})
-  }).catch((e) => {
-    bot.sendMessage(chatId, STRING_ERROR_KRAKEN, {parse_mode : "html"});    
-  })
+  try {
+    const value = await sendRequest("EUR");
+    if (value !== undefined)
+      bot.sendMessage(chatId, STRING_RESPONSE_XMR + value.EUR + " €</b>", {parse_mode : "html"})
+    else
+      bot.sendMessage(chatId, STRING_ERROR, {parse_mode : "html"});
+  } catch (e) {
+    bot.sendMessage(chatId, STRING_ERROR, {parse_mode : "html"});    
+    
+  }
 })
 
-function krakenXMREURWrapped() {
-    return new Promise((resolve, reject) => {
-      kraken.api('Ticker', { pair: 'XMREUR' }, (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          const arr = Object.keys(data.result.XXMRZEUR);
-          arr.forEach((i) => {
-            if (i === 'c') {
-              resolve(parseFloat(data.result.XXMRZEUR[i][0]));
-            }
-          });
-        }
-      });
-    });
+
+bot.onText(/\/usd/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  try {
+    const value = await sendRequest("USD");
+    if (value !== undefined)
+      bot.sendMessage(chatId, STRING_RESPONSE_XMR + value.USD + " $</b>", {parse_mode : "html"})
+    else
+      bot.sendMessage(chatId, STRING_ERROR, {parse_mode : "html"});
+  } catch (e) {
+    bot.sendMessage(chatId, STRING_ERROR, {parse_mode : "html"});        
   }
+})
+
+
+bot.onText(/\/btc/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  try {
+    const value = await sendRequest("BTC");
+    if (value !== undefined)
+      bot.sendMessage(chatId, STRING_RESPONSE_XMR + value.BTC + " BTC</b>", {parse_mode : "html"})
+    else
+      bot.sendMessage(chatId, STRING_ERROR, {parse_mode : "html"});
+  } catch (e) {
+    bot.sendMessage(chatId, STRING_ERROR, {parse_mode : "html"});        
+  }
+})
+
+
+function sendRequest(fiat) {
+  return new Promise(function (resolve, reject) {
+    https.get(standardQuery + fiat.toUpperCase(), (res) => {
+      let data = '';
+      res.on('data', (change) => {
+        data += change;
+      });
+      res.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    }).on("error", (err) => {
+        reject(err);
+    });
+  });
+}
